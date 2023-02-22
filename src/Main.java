f3f012796a1b351aa21487deea7625ccc839d3f4
Anastasiya Pomogaeva <pomogaeva.anasyasiya@gmail.com>
2023-02-22 21:15:24 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Person> generateClients(){
+            //TODO возвращать список из 5 людей
+        
+        }
+
+        Queue<Person> queue = new LinkedList<>();
+        // TODO заполнить людьми из generateClients()
+
+        while(!queue.isEmpty()){
+            // TODO 1) вытащить из очереди человека;
+            //  2) вывести сообщение;
+            //  3) уменьшить кол-во билетов у человека на 1;
+            //  4) если биелетов не 0, поставить человека в конец очереди.
+        }
+    }
+}
